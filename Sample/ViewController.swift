25ce46b5331f4cfc4da9adcda51e917bc5f1bcd5
shika-e <Kento-E@users.noreply.github.com>
2020-08-27 16:30:02 +0900

    [add] Button Events.

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 0, y: 50, width: 100, height: 50)
+        button.setTitle("Clear", for: .normal)
+        button.addTarget(self, action: #selector(self.clearHello(_:)), for: .touchUpInside)
+        self.view.addSubview(button)
     }
 
-
+    @IBAction func showHello(_ sender: Any) {
+        self.textLabel.text = "Hello!"
+    }
+    
+    @objc func clearHello (_ sender: Any) {
+        self.textLabel.text = "Welcome!"
+    }
 }
-
